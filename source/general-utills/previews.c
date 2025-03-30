de062348d01935c7229e509903c8d5337b0fef17
loglot <88983354+loglot@users.noreply.github.com>
2025-03-30 14:39:33 +0000

    pushing to cloud

--- source/general-utills/previews.c
+++ source/general-utills/previews.c
@@ -5,18 +5,18 @@ void YAFGPreviewTop(){
     int offsetXBG=offsetX+pos;
     int offsetY=(240-(240*scaleY))/2;
 
-    DrawRect(0+offsetXBG, 0+offsetY, 400, 240, YABlue,0);
-    DrawRect(195+offsetXBG, 0+offsetY, 10, 240, YAOL,0);
-    DrawRect(105+offsetXBG, 0+offsetY, 10, 240, YAOL,0);
-    DrawRect(285+offsetXBG, 0+offsetY, 10, 240, YAOL,0);
-    DrawRect(15+offsetXBG, 0+offsetY, 10, 240, YAOL,0);
-    DrawRect(375+offsetXBG, 0+offsetY, 10, 240, YAOL,0);
+    C2D_DrawRectSolid(0+offsetXBG, 0+offsetY, 400, 240, YABlue,0);
+    C2D_DrawRectSolid(195+offsetXBG, 0+offsetY, 10, 240, YAOL,0);
+    C2D_DrawRectSolid(105+offsetXBG, 0+offsetY, 10, 240, YAOL,0);
+    C2D_DrawRectSolid(285+offsetXBG, 0+offsetY, 10, 240, YAOL,0);
+    C2D_DrawRectSolid(15+offsetXBG, 0+offsetY, 10, 240, YAOL,0);
+    C2D_DrawRectSolid(375+offsetXBG, 0+offsetY, 10, 240, YAOL,0);
 
-    DrawRect(2*90-25+pos*1.5, 200+pos/2, 90, 35, YAOL,0);
+    C2D_DrawRectSolid(2*90-25+pos*1.5, 200+pos/2, 90, 35, YAOL,0);
 
-    DrawCirc(200-5+offsetX+pos*2, 100+5+offsetY*2, 30, YAShadow,0);
-    DrawCirc(200+offsetX+pos*2, 100+offsetY*2, 30, YAOL,0);
-    DrawCirc(200+offsetX+pos*2, 100+offsetY*2, 25, YAGreen,0);
+    C2D_DrawCircleSolid(200-5+offsetX+pos*2, 100+5+offsetY*2, 30, YAShadow,0);
+    C2D_DrawCircleSolid(200+offsetX+pos*2, 100+offsetY*2, 30, YAOL,0);
+    C2D_DrawCircleSolid(200+offsetX+pos*2, 100+offsetY*2, 25, YAGreen,0);
 }
 void YAFGPreviewBottom(){
     int offset[2]={-40,-320};
